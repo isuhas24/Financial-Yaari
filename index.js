c7e4f0fad4ea7696749fd4b503cfbf6d6e1ed382
isuhas24 <86099959+isuhas24@users.noreply.github.com>
2021-11-17 19:20:33 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function faq1_function(){
+    var div1=document.getElementById("faq1");
+    if (div1.style.display=="none"){
+        div1.style.display="block";
+    }
+    else{
+        div1.style.display="none";
+    }
+}
+
+function faq2_function(){
+    var div2=document.getElementById("faq2");
+    if (div2.style.display=="none"){
+        div2.style.display="block";
+    }
+    else{
+        div2.style.display="none";
+    }
+}
+
+function faq3_function(){
+    var div3=document.getElementById("faq3");
+    if (div3.style.display=="none"){
+        div3.style.display="block";
+    }
+    else{
+        div3.style.display="none";
+    }
+}
